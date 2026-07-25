e68e6d8ca357b2883dbd788b23530018fbeb68e9
Luca Piai <lucapiai98@proton.me>
2026-07-25 18:14:46 +0200

    Added new prompts (backup)

--- data_preparation/create_testset.py
+++ data_preparation/create_testset.py
@@ -15,6 +15,10 @@ PROMPTS = {
         "add a crack to the surface of the hazelnut",
         "add dark rot spots to the hazelnut",
         "add burned surface spots to the hazelnut",
+        "crushed hull",
+        "add surface abrasions",
+        "add surface mold",
+        "add blue dots"
 
         #"add a deep scratch to the hazelnut",
         #"add a dark liquid stain to the hazelnut",
@@ -56,8 +60,7 @@ PROMPTS = {
         "a cable with rodent damage",
         "exposed conductor",
         "frayed strands"
-
-        #"cable swap: two brown wires one green/yellow wire",
+        #"cables swap: two green one brown",
     ],
     "transistor" : [
         "add an exploded package to the transistor",
@@ -67,7 +70,8 @@ PROMPTS = {
         "a transistor with a chipped edge",
         "cracked case",
         "chipped housing",
-        "lead discoluration"
+        "lead discoluration",
+        "snapped terminal"
     ],
     "tile" : [
         "add a chemical stain to the tile",
@@ -79,7 +83,8 @@ PROMPTS = {
         "add a puncture hole to the leather",
         "add mold or mildew to the leather",
         "add a liquid stain to the leather",
-        "add a cigarette burn to the leather"
+        "add a cigarette burn to the leather",
+        "add a blue ink stain",
     ],
     "wood" : [
         "add hole",
@@ -90,7 +95,11 @@ PROMPTS = {
         "deep scratches",
         "burned black dots",
         "discoloration",
-        "chemical stain"
+        "chemical stain",
+        "rodent gnawing",
+        "insect bore hole",
+        "surface abrasions",
+        "chipped edge"
     ],
     "carpet" : [
         "puncture hole",
@@ -99,7 +108,8 @@ PROMPTS = {
         "deep scratches",
         "jagged tear",
         "discoloration",
-        "chemical stain"
+        "chemical stain",
+        "add a blue ink stain",
     ],
     "capsule" : [
         "defected printing",
@@ -109,20 +119,34 @@ PROMPTS = {
         "corroded surface",
         "chipped edge",
         "exploded capsule",
-        "add mold"
+        "add mold",
+        "add an embedded contaminant",
+        "rusted surface"
     ], 
-    "metal_nut": [
-        "deep scratches on the surface",
-        "burred rim",
-        "peeling plating",
-        "corroded surface",
-        "hairline crack",
-        "add a small hole",
-        "add a chipped edge",
-        "exploded metal nut",
-        "add a liquid stain",
-        "bloated shape"
-    ]
+    # The categories below belongs to VISA!
+    #"metal_nut": [
+    #    "deep scratches on the surface",
+    #    "burred rim",
+    #    "peeling plating",
+    #    "corroded surface",
+    #    "hairline crack",
+    #    "add a small hole",
+    #    "add a chipped edge",
+    #    "exploded metal nut",
+    #    "add a liquid stain",
+    #    "bloated shape",
+    #    "rusted surface"
+    #],
+    #"candle": [
+    #    "add a melted wax drip to the circles",
+    #    "add a crack to each circle surface",
+    #    "add a burn mark to the circles",
+    #    "add a discoloration to the circles",
+    #    "add a chip to the circles",
+    #    "add a dent to the circles",
+    #    "add a scratch to the circles",
+    #    "add a stain to the circles"
+    #],
 }
 
 
